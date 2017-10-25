4a0cdf21c87e5d9d65dec4a2ae90f3629145afe3
theWickedWebDev <syoung@paintnite.com>
2017-10-25 19:02:42 -0400

    First Commit

---
+++ footer.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+		</div><!-- #content -->
+
+
+<footer class="main-footer clearfix">
+
+  <div class="right">
+    <ul>
+      <li><a href="contact">Contact</a></li>
+      <li><a href="careers">Careers</a></li>
+      <li><a href="blog">Blog</a></li>
+      <li><a href="data-sheets">Product Data Sheets</a></li>
+      <li><a href="conflict-minerals-policy">Conflict Minerals Policy</a></li>
+      <li><a href="iso9001">ISO 9001</a></li>
+      <li><a href="terms">Terms & Conditions</a></li>
+    </ul>
+  </div>
+  <div class="left">
+    <p><span>CMI</span> Central Midori International</p>
+    <p>Singapore | Germany | USA</p>
+    <p class="small">&copy; 2017 Central-Midori (Int'l) Pte Ltd. All Rights Reserved</p>
+  </div>
+</footer>
+
+	</div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
+
+
+<style>
+.main-footer {
+  background-color: #2e70a8;
+  box-shadow: 0 50px 50px inset #000;
+  color: rgba(255, 255, 255, 0.68);
+  padding: 60px 20px; }
+  .main-footer .left {
+    margin-top: 100px;
+    display: block;
+    text-align: center; }
+    .main-footer .left p {
+      margin: 0 0 10px;
+      font-size: 12px;
+      line-height: 22px; }
+      .main-footer .left p span {
+        color: rgba(255, 255, 255, 0.88);
+        font-weight: 600; }
+    .main-footer .left .small {
+      font-size: 10px; }
+  .main-footer .right ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    font-size: 14px;
+    display: block;
+    text-align: center; }
+    .main-footer .right ul li {
+      display: inline-block;
+      box-sizing: border-box;
+      padding: 0;
+      margin: 0 10px 20px; }
+    .main-footer .right ul a {
+      text-decoration: none;
+      color: #fff; }
+</style>
---
+++ template-parts/header/header-image.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<nav class="main-nav-bar">
+  <a href="/">
+    <img class="logo" src="<?php echo get_template_directory_uri() ?>/assets/images/logo.png"/>
+  </a>
+  <ul class="main-list">
+    <li><a href="/" id="homeLink">Home</a></li>
+    <li><a href="products" id="productLink">Products</a></li>
+    <li><a href="services" id="servicesLink">Services</a></li>
+    <li><a href="manufacturing" id="manufacturingLink">Manufacturing</a></li>
+    <li><a href="company" id="companyLink">Company</a></li>
+  </ul>
+  <div class="phone">
+    <i class="fa fa-phone" aria-hidden="true"></i>
+    <a href="/contact">Contact</a>
+  </div>
+  <img class="bars" onClick="toggleMobileNav()" src="<?php echo get_template_directory_uri() ?>/assets/images/bars.png"/>
+  <div class="slide-menu hidden" id="slideMenu">
+
+  <ul class="main-list">
+    <li><a href="/" id="homeLink">Home</a></li>
+    <li><a href="products" id="productLink">Products</a></li>
+    <li><a href="services" id="servicesLink">Services</a></li>
+    <li><a href="manufacturing" id="manufacturingLink">Manufacturing</a></li>
+    <li><a href="company" id="companyLink">Company</a></li>
+  </ul>
+    <div class="underlay" onClick="toggleMobileNav()"></div>
+  </div>
+</nav>
+
+<script>
+  var toggleMobileNav = function() {
+    console.log('BANG');
+    var menu = $('#slideMenu');
+    if (menu.hasClass('hidden')) {
+      menu.addClass('visible');
+      menu.removeClass('hidden');
+    } else {
+      menu.addClass('hidden');
+      menu.removeClass('visible');
+    }
+  }
+</script>
+
+<style>
+.main-nav-bar {
+  background-color: rgba(255, 255, 255, 0.93);
+  padding: 10px 40px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 999;
+  box-sizing: border-box;
+  border-bottom: solid 1px #f6f7f9; }
+  .main-nav-bar a {
+    text-decoration: none; }
+  .main-nav-bar .logo {
+    height: 45px;
+    display: inline-block;
+    vertical-align: middle;
+    margin-right: 60px; }
+  .main-nav-bar .main-list {
+    margin: 0;
+    display: inline-block;
+    vertical-align: middle; }
+    .main-nav-bar .main-list li {
+      display: inline-block;
+      margin: 0 40px 0 0; }
+    .main-nav-bar .main-list a {
+      text-decoration: none;
+      text-transform: uppercase;
+      font-size: 14px;
+      color: #3a3a3a;
+      font-weight: 600;
+      position: relative;
+      transition: ease all .4s; }
+      .main-nav-bar .main-list a:after {
+        transition: ease all .4s;
+        opacity: 0;
+        position: absolute;
+        bottom: -22px;
+        display: block;
+        content: " ";
+        height: 4px;
+        width: 100%;
+        background-color: #d62c1b; }
+      .main-nav-bar .main-list a:hover {
+        color: #d62c1b; }
+        .main-nav-bar .main-list a:hover:after {
+          opacity: 1; }
+      .main-nav-bar .main-list a.active {
+        color: #d62c1b; }
+        .main-nav-bar .main-list a.active:after {
+          opacity: 1; }
+  .main-nav-bar .phone {
+    float: right;
+    position: relative;
+    top: 3px;
+    background-color: #2e70a8;
+    padding: 10px 15px;
+    border-radius: 20px; }
+    .main-nav-bar .phone a,
+    .main-nav-bar .phone .fa-phone {
+      display: inline-block;
+      vertical-align: middle;
+      color: #fff; }
+    .main-nav-bar .phone .fa-phone {
+      margin-right: 5px; }
+    .main-nav-bar .phone a {
+      font-weight: 600;
+      cursor: pointer;
+      font-size: 16px;
+      text-decoration: none; }
+  .main-nav-bar .bars {
+    display: none; }
+
+@media screen and (min-width: 780px) {
+  .slide-menu {
+    display: none; } }
+@media screen and (max-width: 1100px) {
+  .main-nav-bar .logo {
+    height: 35px;
+    margin-right: 15px; }
+  .main-nav-bar .main-list a {
+    font-size: 12px; }
+  .main-nav-bar .main-list li {
+    margin-right: 20px; }
+  .main-nav-bar .phone {
+    top: 3px; }
+  .main-nav-bar .phone a {
+    font-size: 14px; } }
+@media screen and (max-width: 840px) {
+  .main-nav-bar {
+    padding-right: 20px; }
+    .main-nav-bar .main-list {
+      display: none; }
+    .main-nav-bar .phone {
+      display: none; }
+    .main-nav-bar .bars {
+      display: block;
+      height: 30px;
+      float: right;
+      position: relative;
+      top: 5px; }
+    .main-nav-bar .slide-menu {
+      position: relative;
+      overflow-y: scroll; }
+      .main-nav-bar .slide-menu .underlay {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background-color: rgba(77, 77, 77, 0.12);
+        cursor: pointer; }
+      .main-nav-bar .slide-menu .main-list {
+        display: block;
+        z-index: 999;
+        position: fixed;
+        top: 0;
+        padding-left: 40px;
+        width: 200px;
+        background-color: #fff;
+        bottom: 0;
+        box-shadow: rgba(0, 0, 0, 0.28) 0 0 8px;
+        transition: ease all .3s;
+        padding-top: 60px; }
+        .main-nav-bar .slide-menu .main-list li {
+          display: block;
+          margin-bottom: 30px; }
+      .main-nav-bar .slide-menu.visible {
+        opacity: 1; }
+        .main-nav-bar .slide-menu.visible .main-list {
+          right: 0; }
+        .main-nav-bar .slide-menu.visible .underlay {
+          display: block; }
+      .main-nav-bar .slide-menu.hidden {
+        opacity: 0; }
+        .main-nav-bar .slide-menu.hidden .underlay {
+          display: none; }
+        .main-nav-bar .slide-menu.hidden .main-list {
+          right: -230px; } }
+</style>
